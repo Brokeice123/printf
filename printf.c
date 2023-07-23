a4312c16b4cf2086fb150e6c95b053e887c57969
Brokeice123 <keithmarkomen@gmail.com>
2023-07-24 00:18:19 +0300

    a function that produces output according to the format c, s, %

---
+++ printf.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/* #<{(|* */
+/*  * print_number - prints a given integer */
+/*  * @n: integer to be printed */
+/*  *Return: i */
+/*  |)}># */
+/*  */
+/* int print_number(int n) */
+/* { */
+/* 	int i = n; */
+/*  */
+/* 	if (n < 0) */
+/* 	{ */
+/* 		_putchar('-'); */
+/* 		i = -i; */
+/* 	} */
+/*  */
+/* 	if ((i / 10) > 0) */
+/* 		print_number(i / 10); */
+/* 	_putchar((i % 10) + '0'); */
+/*  */
+/* 	return (i); */
+/* } */
+
+/**
+ *_printf -  produces output according to a format
+ *@format: a character string
+ *
+ *Return: Count
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+	char *s;
+
+	va_start(args, format);
+	for (; *format != '\0'; format++)
+	{
+		if (*format == '%')
+		{
+			switch (*++format)
+			{
+				case 'c':
+				count += _putchar(va_arg(args, int));
+				break;
+				case 's':
+				s = va_arg(args, char *);
+				count += write(1, s, strlen(s));
+				break;
+				case '%':
+				count += write(1, "%", 1);
+				break;
+				/* case 'd': */
+				/* case 'i': */
+				/* num = va_arg(args, int); */
+				/* count += print_number(num); */
+				/* break; */
+				default:
+				break;
+			}
+		} else
+		{
+		count += write(1, format, 1);
+		}
+	}
+	va_end(args);
+	return (count);
+}
