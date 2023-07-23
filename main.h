0ed19d7330987c86a9e4b034e4a7a69a144b9d0e
Brokeice123 <keithmarkomen@gmail.com>
2023-07-24 01:34:46 +0300

    a function that produces output according to the format c, s, %

--- main.h
+++ main.h
@@ -1,7 +1,14 @@
 #ifndef PRINTF_H
 #define PRINTF_H
 
-int _printf(const char *format, ...);
+#include <stdarg.h>
+#include <unistd.h>
+#include <stddef.h>
 
+int _printf(const char *format, ...);
+int _write(const char *str, int len);
+int char_fmt(char c);
+int string_fmt(char *s);
+int percent_fmt(void);
 
 #endif
--- printf.c
+++ printf.c
@@ -1,36 +1,4 @@
-#include <stdlib.h>
-#include <stdarg.h>
-#include <string.h>
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- *_strlen - Returns the length of a string
- *@s: The string being used
- *Return: Returns the length of the string
- */
-
-int _strlen(char *s)
-{
-	unsigned int len = 0;
-	int i;
-
-	for (i = 0; *(s + i) != '\0'; i++)
-		len += sizeof(char);
-
-	return (len);
-}
+#include "main.h"
 
 /**
  *_printf -  produces output according to a format
@@ -43,7 +11,7 @@ int _printf(const char *format, ...)
 {
 	int count = 0;
 	va_list args;
-	char *s;
+	char c, *s;
 
 	va_start(args, format);
 	for (; *format != '\0'; format++)
@@ -53,21 +21,23 @@ int _printf(const char *format, ...)
 			switch (*++format)
 			{
 				case 'c':
-				count += _putchar(va_arg(args, int));
+				c = (char) va_arg(args, int);
+				count += char_fmt(c);
 				break;
 				case 's':
 				s = va_arg(args, char *);
-				count += write(1, s, _strlen(s));
+				count += string_fmt(s);
 				break;
 				case '%':
-				count += write(1, "%", 1);
+				count += percent_fmt();
 				break;
 				default:
 				break;
 			}
-		} else
+		}
+		else
 		{
-		count += write(1, format, 1);
+			_write(format, 1);
 		}
 	}
 	va_end(args);
